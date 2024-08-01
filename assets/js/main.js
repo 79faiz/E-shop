9d837576180cda1b75b82fa299c592b7a13681ae
Faiz Khan <166205231+79faiz@users.noreply.github.com>
2024-08-02 02:03:58 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,7 @@ let minItem = document.querySelector("#min");
 let secItem = document.querySelector("#sec");
 
 let countDown = () => {
-    let futureDate = new Date("10 July 2024");
+    let futureDate = new Date("5 August 2024");
     let currentDate = new Date();
     let myDate = futureDate - currentDate;
 
